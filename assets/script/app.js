f1db1a2740386be5f0ce83d38f3958efe9517fd6
Olisa Nweze <nwezeolisa@gmail.com>
2024-03-11 11:08:07 -0500

    Style edits

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,11 +2,25 @@
 
 const menuButton = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav');
+const mobileMenuLinks = document.querySelector('.mobile-nav-link');
 
-window.onload = function() {
-    menuButton.addEventListener('click', function() {
-        menuButton.classList.toggle('is-active');
-        mobileMenu.classList.toggle('is-active');
-    })
-}
+const allLinks = document.querySelectorAll('.mobile-nav-link');
+
+allLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('is-active');
+        menuButton.classList.remove('is-active');
+    });
+});
+
+
+menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+});
+
+mobileMenuLinks.addEventListener('click', function() {
+    mobileMenu.classList.remove('is-active');
+    menuButton.classList.remove('is-active');
+});
 
