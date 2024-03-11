e73f00bb3f784f8b426a371c5ef9cb0d56a1f382
Olisa Nweze <nwezeolisa@gmail.com>
2024-03-11 12:19:19 -0500

    Visual changes

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,8 +2,6 @@
 
 const menuButton = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav');
-const mobileMenuLinks = document.querySelector('.mobile-nav-link');
-
 const allLinks = document.querySelectorAll('.mobile-nav-link');
 
 allLinks.forEach(link => {
@@ -18,9 +16,3 @@ menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
 });
-
-mobileMenuLinks.addEventListener('click', function() {
-    mobileMenu.classList.remove('is-active');
-    menuButton.classList.remove('is-active');
-});
-
